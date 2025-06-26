d0608b95a27f293b05cb5a506a9e9f852cd85a36
Michael Cruz <84043530+carnivalcruz380@users.noreply.github.com>
2025-06-26 16:27:24 -0400

    added single message funcationality

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,6 +1,15 @@
-import { forwardRef } from "react"
+import { forwardRef, useState } from "react"
 import sendMail from "../script";
+
 const Message = forwardRef(function Message(props, ref) {
+    const [isSent, setIsSent] = useState(false);
+
+    function handleSend(e) {
+        e.preventDefault();
+        sendMail();
+        setIsSent(true);
+    }
+
     return (
         <section className="h-auto mt-10 flex justify-center items-center bg-white  text-black p-8 gap-12" ref={ref}>
             <div className="flex flex-col items-center w-1/3 bg-gray-100 p-6 rounded-lg shadow-lg">
@@ -9,7 +18,7 @@ const Message = forwardRef(function Message(props, ref) {
                     To inquire about my services, discuss potential collaborations or job opportunities, or just to say hello, feel free to reach out via email. I look forward to hearing from you!
                 </p>
             </div>
-            <form className="mt-8 w-2/3">
+            {!isSent && <form className="mt-8 w-2/3">
                 <div className="flex flex-col gap-4">
                     <input 
                         type="text" 
@@ -40,16 +49,19 @@ const Message = forwardRef(function Message(props, ref) {
                     ></textarea>
                     <button 
                         type="submit" 
-                        onClick={(e) => {
-                            e.preventDefault();
-                            sendMail();
-                        }}
+                        onClick={handleSend}
                         className="bg-blue-500 text-white p-2 rounded-2xl hover:bg-blue-600 transition duration-200"
                     >
                         Send Message
                     </button>
                 </div>
-            </form>
+            </form>}
+            {isSent && <div className="flex flex-col items-center w-2/3 bg-green-100 p-6 rounded-lg shadow-lg">
+                <h1 className="text-4xl font-bold mb-4">Message Sent!</h1>
+                <p className="text-lg text-center max-w-2xl">
+                    Thank you for reaching out! I will get back to you as soon as possible.
+                </p>
+            </div>}
         </section>
     )
 });
