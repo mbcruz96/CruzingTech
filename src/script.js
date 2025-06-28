606df32b11109d3088b56e1cef722dbcd960732d
Michael Cruz <84043530+carnivalcruz380@users.noreply.github.com>
2025-06-28 17:17:45 -0400

    fixed env variable problem

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
- export default function sendMail(recaptcha_value){
+import emailjs from '@emailjs/browser';
+
+export default function sendMail(recaptcha_value){
     const params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -6,9 +8,15 @@
         message: document.getElementById("message").value,
         'g-recaptcha-response': recaptcha_value,
     }
+
+    const email_key = import.meta.env.VITE_EMAILJS_KEY;
     const service_id = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const template_id = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-    emailjs.send(service_id, template_id, params).then(
+    
+    emailjs.send(service_id, template_id, params, {
+        publicKey: email_key,
+    })
+    .then(
         alert("Message sent successfully!"))
         .catch(err => console.error("Failed to send message. Error: ", err));
 };
