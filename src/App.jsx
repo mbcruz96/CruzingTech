505f36c76d25dcee5ffc883b5d903e8498d641e7
Michael Cruz <84043530+carnivalcruz380@users.noreply.github.com>
2025-07-07 12:49:25 -0400

    added visit analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react"
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import { Analytics } from '@vercel/analytics/next';
 import Main from "./components/main"
 import Blog from "./components/blog"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   )
 };
